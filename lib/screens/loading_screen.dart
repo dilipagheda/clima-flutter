aa8d4a25ebca8ab72ef66982befab64726e49de8
Dilip <dilip_agheda@yahoo.com>
2019-05-25 13:49:36 +1000

    Used methods to get location and weather data in loading screen for testing

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,8 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:clima/utilities/weather_data.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +10,31 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  WeatherData weatherData;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocationAndData();
+  }
+
+  void getLocationAndData() async {
+    //Get the current location
+    Position position = await Location.getLocation();
+    WeatherData weatherData = await FetchData.getData(position);
+    setState(() {
+      this.weatherData = weatherData;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocationAndData();
           },
           child: Text('Get Location'),
         ),
