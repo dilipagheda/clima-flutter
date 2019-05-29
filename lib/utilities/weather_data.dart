1b6ee92dd1a6a21e6f3cedda7486bd3ff07e031e
Dilip <dilip_agheda@yahoo.com>
2019-05-29 20:55:05 +1000

    Added getter for conversion from fareinheit to celcius

--- lib/utilities/weather_data.dart
+++ lib/utilities/weather_data.dart
@@ -1,6 +1,15 @@
 class WeatherData {
   int condition;
-  int temp;
+  int _temp;
   String city;
-  WeatherData({this.condition, this.temp, this.city});
+
+  int get temp {
+    double v = (_temp - 32) * (5 / 9);
+    return v.round();
+  }
+
+  WeatherData({int condition, int temp, String city})
+      : this.condition = condition,
+        this._temp = temp,
+        this.city = city;
 }
